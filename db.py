3149738c8802131376a1ca08861ebbe8fa53b366
Pierre Baonla <pbaonla@octo.com>
2020-01-23 17:43:46 +0100

    (PC-2820) add review comment

--- db.py
+++ db.py
@@ -2,10 +2,8 @@ import os
 
 from sqlalchemy import create_engine
 
-# By default DATABASE_URL is set to localhost in order to execute tests in local
-# For others environments DATABASE_URL should be set as env variable
-
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://pass_culture:passq@localhost:5435/pass_culture')
+LOCAL_DATABASE_URL = 'postgresql://pass_culture:passq@localhost:5435/pass_culture'
+DATABASE_URL = os.environ.get('DATABASE_URL', LOCAL_DATABASE_URL)
 
 ENGINE = create_engine(DATABASE_URL)
 CONNECTION = ENGINE.connect()
