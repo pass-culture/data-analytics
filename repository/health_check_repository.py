fd8472ba6de648e13d12d3040fd5c9eb19935a1d
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-03-04 15:02:48 +0100

    (PC-3010): Create an assert on the stock view

--- repository/health_check_repository.py
+++ repository/health_check_repository.py
@@ -15,6 +15,9 @@ def is_enriched_user_data_exists():
     return len(results) == 1
 
 
-def check_enriched_stock_data_exists():
-    pass
+def is_enriched_stock_data_exists():
+    query = '''select * from information_schema.tables WHERE table_name = 'enriched_stock_data';'''
+    results = db.session.execute(query).fetchall()
+
+    return len(results) == 1
 
--- tests/repository/health_check_repository_test.py
+++ tests/repository/health_check_repository_test.py
@@ -1,11 +1,13 @@
-from unittest.mock import MagicMock, patch
-
 import pytest
 
 from db import CONNECTION
+from db import db
+
 from query_enriched_data_tables import create_enriched_offerer_data, create_enriched_user_data
-from repository.health_check_repository import is_enriched_offerer_data_exists, is_enriched_user_data_exists
+from repository.health_check_repository import is_enriched_offerer_data_exists, is_enriched_user_data_exists, \
+    is_enriched_stock_data_exists
 from tests.utils import clean_database
+from view_queries import create_enriched_stock_view
 
 
 class IsEnrichedOffererDataExistsTest:
@@ -51,6 +53,31 @@ class IsEnrichedUserDataExistsTest:
 
         # When
         result = is_enriched_user_data_exists()
+
+        # Then
+        assert result is True
+
+
+class IsEnrichedStockDataExistsTest:
+    @pytest.fixture(autouse=True)
+    def setup_class(self, app):
+        clean_database(app)
+
+    def test_should_return_false_when_no_table_exists(self, app):
+        # When
+        result = is_enriched_stock_data_exists()
+
+        # Then
+        assert result is False
+
+
+    def test_should_return_true_when_table_exists(self, app):
+        # Given
+        with app.app_context():
+            create_enriched_stock_view()
+
+        # When
+        result = is_enriched_stock_data_exists()
 
         # Then
         assert result is True
