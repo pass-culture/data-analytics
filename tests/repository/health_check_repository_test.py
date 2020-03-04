a3935780da3d9e66ac41c07baf7e5fb4d5ffd977
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-03-04 16:51:05 +0100

    (PC-3010): Fix tests

--- tests/repository/health_check_repository_test.py
+++ tests/repository/health_check_repository_test.py
@@ -1,11 +1,9 @@
 import pytest
 
-from db import CONNECTION
-from db import db
-
+from db import CONNECTION, db
 from query_enriched_data_tables import create_enriched_offerer_data, create_enriched_user_data
-from repository.health_check_repository import is_enriched_offerer_data_exists, is_enriched_user_data_exists, \
-    is_enriched_stock_data_exists
+from repository.health_check_repository import is_enriched_stock_data_exists, is_enriched_offerer_data_exists, \
+    is_enriched_user_data_exists
 from tests.utils import clean_database
 from view_queries import create_enriched_stock_view
 
@@ -62,6 +60,11 @@ class IsEnrichedStockDataExistsTest:
     @pytest.fixture(autouse=True)
     def setup_class(self, app):
         clean_database(app)
+        with app.app_context():
+            db.session.execute('''
+                DROP VIEW IF EXISTS enriched_stock_data;
+            ''')
+            db.session.commit()
 
     def test_should_return_false_when_no_table_exists(self, app):
         # When
