b0b193adce3310ad433bb1ecde0807662f69ae61
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-03-04 18:03:33 +0100

    (PC-3010): Expose status on health

--- repository/health_check_repository.py
+++ repository/health_check_repository.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from db import db
 
 
--- tests/application/get_enriched_data_status_test.py
+++ tests/application/get_enriched_data_status_test.py
@@ -5,54 +5,234 @@ from application.get_enriched_data_status import get_enriched_data_status
 
 class GetEnrichedDataStatusTest:
 
-    def test_should_return_a_dict_with_offerer_table_status(self):
-        # Given
-        is_enriched_stock_data_exists = MagicMock()
-        is_enriched_user_data_exists = MagicMock()
-        is_enriched_offerer_data_exists = MagicMock()
-        is_enriched_offerer_data_exists.return_value = True
+    class OffererStatusTest:
+        def test_should_return_a_dict_with_offerer_table_status(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
 
-        # When
-        status = get_enriched_data_status(
-            is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
-            is_enriched_user_data_exists=is_enriched_user_data_exists,
-            is_enriched_stock_data_exists=is_enriched_stock_data_exists
-        )
+            is_enriched_offerer_data_exists.return_value = True
 
-        # Then
-        assert status['is_enriched_offerer_datasource_exists'] == True
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
 
-    def test_should_return_a_dict_with_user_status(self):
-        # Given
-        is_enriched_stock_data_exists = MagicMock()
-        is_enriched_offerer_data_exists = MagicMock()
-        is_enriched_user_data_exists = MagicMock()
-        is_enriched_user_data_exists.return_value = False
+            # Then
+            assert status['is_enriched_offerer_datasource_exists'] == True
 
-        # When
-        status = get_enriched_data_status(
-            is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
-            is_enriched_user_data_exists=is_enriched_user_data_exists,
-            is_enriched_stock_data_exists=is_enriched_stock_data_exists
-        )
+        def test_should_return_is_offerer_ok_as_true_when_table_exists_with_data(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
 
-        # Then
-        assert status['is_enriched_user_datasource_exists'] == False
+            is_enriched_offerer_data_exists.return_value = True
+            is_enriched_offerer_contains_data.return_value = True
+
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_offerer_datasource_exists'] == True
+            assert status['is_offerer_ok'] == True
+
+        def test_should_return_is_offerer_ok_as_false_when_table_exists_without_data(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
+
+            is_enriched_offerer_data_exists.return_value = True
+            is_enriched_offerer_contains_data.return_value = False
+
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_offerer_datasource_exists'] == True
+            assert status['is_offerer_ok'] == False
+
+    class UserStatusTest:
+        def test_should_return_a_dict_with_user_table_status(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
+            is_enriched_user_data_exists.return_value = False
+
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_user_datasource_exists'] == False
+
+        def test_should_return_is_user_ok_as_true_when_table_exists_with_data(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
+
+            is_enriched_user_data_exists.return_value = True
+            is_enriched_users_contains_data.return_value = True
+
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_user_datasource_exists'] == True
+            assert status['is_user_ok'] == True
+
+        def test_should_return_is_user_ok_as_false_when_table_exists_without_data(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
+
+            is_enriched_user_data_exists.return_value = True
+            is_enriched_users_contains_data.return_value = False
+
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_user_datasource_exists'] == True
+            assert status['is_user_ok'] == False
+
+    class StockStatusTest:
+        def test_should_return_a_dict_with_stock_status(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
+
+            is_enriched_stock_data_exists.return_value = True
+
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_stock_datasource_exists'] == True
 
 
-    def test_should_return_a_dict_with_stock_status(self):
-        # Given
-        is_enriched_offerer_data_exists = MagicMock()
-        is_enriched_user_data_exists = MagicMock()
-        is_enriched_stock_data_exists = MagicMock()
-        is_enriched_stock_data_exists.return_value = True
+        def test_should_return_is_stock_ok_as_true_when_table_exists_with_data(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
 
-        # When
-        status = get_enriched_data_status(
-            is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
-            is_enriched_user_data_exists=is_enriched_user_data_exists,
-            is_enriched_stock_data_exists=is_enriched_stock_data_exists
-        )
+            is_enriched_stock_data_exists.return_value = True
+            is_enriched_stocks_contains_data.return_value = True
 
-        # Then
-        assert status['is_enriched_stock_datasource_exists'] == True
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_stock_datasource_exists'] == True
+            assert status['is_stock_ok'] == True
+
+
+        def test_should_return_is_stock_ok_as_true_when_table_exists_without_data(self):
+            # Given
+            is_enriched_offerer_contains_data = MagicMock()
+            is_enriched_users_contains_data = MagicMock()
+            is_enriched_stocks_contains_data = MagicMock()
+            is_enriched_stock_data_exists = MagicMock()
+            is_enriched_user_data_exists = MagicMock()
+            is_enriched_offerer_data_exists = MagicMock()
+
+            is_enriched_stock_data_exists.return_value = True
+            is_enriched_stocks_contains_data.return_value = False
+
+            # When
+            status = get_enriched_data_status(
+                is_enriched_offerer_data_exists=is_enriched_offerer_data_exists,
+                is_enriched_user_data_exists=is_enriched_user_data_exists,
+                is_enriched_stock_data_exists=is_enriched_stock_data_exists,
+                is_enriched_offerer_contains_data=is_enriched_offerer_contains_data,
+                is_enriched_users_contains_data=is_enriched_users_contains_data,
+                is_enriched_stocks_contains_data=is_enriched_stocks_contains_data
+            )
+
+            # Then
+            assert status['is_enriched_stock_datasource_exists'] == True
+            assert status['is_stock_ok'] == False
