64de4d408179f4f2e19f2cc6fdfca5a5b1dca8ea
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2020-03-04 17:02:35 +0100

    (PC-3010): Check if table offerer contains data

--- repository/health_check_repository.py
+++ repository/health_check_repository.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from db import db
 
 
@@ -8,11 +10,31 @@ def is_enriched_offerer_data_exists():
     return len(results) == 1
 
 
+def is_enriched_offerer_contains_data():
+
+    if is_enriched_offerer_data_exists():
+        query = 'SELECT COUNT(*) FROM enriched_offerer_data'
+        results = db.session.execute(query)
+        return results.rowcount > 0
+
+    return False
+
+
 def is_enriched_user_data_exists():
     query = '''select * from information_schema.tables WHERE table_name = 'enriched_user_data';'''
     results = db.session.execute(query).fetchall()
 
     return len(results) == 1
+
+
+def is_enriched_users_contains_data():
+
+    if is_enriched_user_data_exists():
+        query = 'SELECT COUNT(*) FROM enriched_user_data'
+        results = db.session.execute(query)
+        return results.rowcount > 0
+
+    return False
 
 
 def is_enriched_stock_data_exists():
@@ -21,3 +43,12 @@ def is_enriched_stock_data_exists():
 
     return len(results) == 1
 
+
+def is_enriched_stocks_contains_data():
+
+    if is_enriched_stock_data_exists():
+        query = 'SELECT * FROM enriched_stock_data'
+        results = db.session.execute(query)
+        return results.rowcount > 0
+
+    return False
--- tests/repository/health_check_repository_test.py
+++ tests/repository/health_check_repository_test.py
@@ -1,10 +1,14 @@
+from pprint import pprint
+
 import pytest
 
 from db import CONNECTION, db
 from query_enriched_data_tables import create_enriched_offerer_data, create_enriched_user_data
 from repository.health_check_repository import is_enriched_stock_data_exists, is_enriched_offerer_data_exists, \
-    is_enriched_user_data_exists
-from tests.utils import clean_database
+    is_enriched_user_data_exists, is_enriched_offerer_contains_data, is_enriched_users_contains_data, \
+    is_enriched_stocks_contains_data
+from tests.utils import clean_database, create_offerer, create_user, create_venue, create_product, create_offer, \
+    create_stock
 from view_queries import create_enriched_stock_view
 
 
@@ -81,6 +85,84 @@ class IsEnrichedStockDataExistsTest:
 
         # When
         result = is_enriched_stock_data_exists()
+
+        # Then
+        assert result is True
+
+
+class IsEnrichedOffererSourceContainsDataTest:
+    @pytest.fixture(autouse=True)
+    def setup_class(self, app):
+        clean_database(app)
+
+    def test_should_return_false_when_contains_no_data(self, app):
+        # When
+        result = is_enriched_offerer_contains_data()
+
+        # Then
+        assert result is False
+
+    def test_should_return_true_when_table_exists_and_contains_an_offerer(self, app):
+        # Given
+        create_offerer(app, id=1)
+        create_enriched_offerer_data(CONNECTION)
+
+        # When
+        result = is_enriched_offerer_contains_data()
+
+        # Then
+        assert result is True
+
+
+class IsEnrichedUserSourceContainsDataTest:
+    @pytest.fixture(autouse=True)
+    def setup_class(self, app):
+        clean_database(app)
+
+    def test_should_return_false_when_contains_no_data(self, app):
+        # When
+        result = is_enriched_users_contains_data()
+
+        # Then
+        assert result is False
+
+    def test_should_return_true_when_table_exists_and_contains_at_least_a_user(self, app):
+        # Given
+        create_user(app, id=1)
+        create_enriched_user_data(CONNECTION)
+
+        # When
+        result = is_enriched_users_contains_data()
+
+        # Then
+        assert result is True
+
+
+class IsEnrichedStocksSourceContainsDataTest:
+    @pytest.fixture(autouse=True)
+    def setup_class(self, app):
+        clean_database(app)
+
+
+    def test_should_return_false_when_contains_no_data(self, app):
+        # When
+        result = is_enriched_stocks_contains_data()
+
+        # Then
+        assert result is False
+
+    def test_should_return_true_when_table_exists_and_contains_at_least_one_stock(self, app):
+        # Given
+        create_offerer(app, id=1)
+        create_venue(app, offerer_id=1, id=1)
+        create_product(app, id=1)
+        create_offer(app, venue_id=1, product_id=1, id=1)
+        create_stock(app, offer_id=1, date_created='2019-12-01')
+        with app.app_context():
+            create_enriched_stock_view()
+
+        # When
+        result = is_enriched_stocks_contains_data()
 
         # Then
         assert result is True
