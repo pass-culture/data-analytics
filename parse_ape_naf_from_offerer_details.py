85d542164a2f6a5cbc793267b2a052f03c9c1e2e
Adrien Lin <adrien.lin@passculture.app>
2020-03-12 11:43:56 +0100

    (PC-3113) Manage APE NAF parsing

--- parse_ape_naf_from_offerer_details.py
+++ parse_ape_naf_from_offerer_details.py
@@ -1,3 +1,5 @@
 def parse_ape_naf_from_offerer_details(offerer_details: dict) -> str:
-
-    return ''
+    try:
+        return offerer_details["unite_legale"]["activite_principale"]
+    except KeyError:
+        return ''
--- tests/parse_ape_naf_from_offerer_details_test.py
+++ tests/parse_ape_naf_from_offerer_details_test.py
@@ -11,3 +11,62 @@ class ParseApeNafFromOffererDetailsTest:
 
         # Then
         assert results == ''
+
+    def test_should_parse_api_result(self):
+        # Given
+        offerer_details = {
+            "unite_legale": {
+                "id": 20493806,
+                "siren": "853318459",
+                "activite_principale": "70.21Z",
+                "etablissement_siege": {
+                    "id": 48863654,
+                    "siren": "853318459",
+                    "siret": "85331845900015",
+                    "activite_principale": "70.21ZX"
+                },
+                "etablissements": [
+                    {
+                        "id": 48863654,
+                        "siren": "853318459",
+                        "siret": "85331845900015",
+                        "activite_principale": "70.21ZZZ"
+                    }
+                ]
+            }
+        }
+
+        # When
+        results = parse_ape_naf_from_offerer_details(offerer_details)
+
+        # Then
+        assert results == "70.21Z"
+
+    def test_should_return_empty_when_APE_is_not_given(self):
+        # Given
+        offerer_details = {
+            "unite_legale": {
+                "id": 20493806,
+                "siren": "853318459",
+                "etablissement_siege": {
+                    "id": 48863654,
+                    "siren": "853318459",
+                    "siret": "85331845900015",
+                    "activite_principale": "70.21ZX"
+                },
+                "etablissements": [
+                    {
+                        "id": 48863654,
+                        "siren": "853318459",
+                        "siret": "85331845900015",
+                        "activite_principale": "70.21ZZZ"
+                    }
+                ]
+            }
+        }
+
+        # When
+        results = parse_ape_naf_from_offerer_details(offerer_details)
+
+        # Then
+        assert results == ""
