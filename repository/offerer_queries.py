a0bf7cca022d1964a7a098527cd903c7952b9c46
faustine <faustine@mbp-de-fam.home>
2020-05-14 12:34:18 +0200

    (PC-3457) Create offerer with department code dataframe

--- repository/offerer_queries.py
+++ repository/offerer_queries.py
@@ -1,6 +1,7 @@
 import pandas
+from models.db import CONNECTION, db
 from pandas import DataFrame
-from models.db import db, CONNECTION
+from repository.offerer_queries import add_department_column_in_offerer_table
 
 
 def _get_first_stock_creation_dates_query() -> str:
