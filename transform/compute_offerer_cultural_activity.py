11d8ef72e79a1d9867448c211963b91d7cacb057
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2020-05-26 10:39:00 +0200

    (PC-3488) changes after review

--- transform/compute_offerer_cultural_activity.py
+++ transform/compute_offerer_cultural_activity.py
@@ -2,11 +2,17 @@ import pandas
 
 from transform.parse_ape_code_from_offerer_details import get_ape_code_by_siren
 
+MAINLAND_DEPARTEMENT_CODE_LENGTH = 2
+OVERSEAS_DEPARTEMENT_CODE_LENGTH = 3
+OVERSEAS_DEPARTEMENT_IDENTIFIER = '97'
+
 
 def get_offerer_cultural_activity_dataframe(siren_dataframe: pandas.DataFrame) -> pandas.DataFrame:
     offerer_cultural_activity_dataframe = siren_dataframe.copy()
-    offerer_cultural_activity_dataframe['APE_code'] = offerer_cultural_activity_dataframe['siren'].apply(get_ape_code_by_siren)
-    offerer_cultural_activity_dataframe['APE_label'] = offerer_cultural_activity_dataframe['APE_code'].apply(get_label_from_given_ape_code)
+    offerer_cultural_activity_dataframe['APE_code'] = offerer_cultural_activity_dataframe['siren'].apply(
+        get_ape_code_by_siren)
+    offerer_cultural_activity_dataframe['APE_label'] = offerer_cultural_activity_dataframe['APE_code'].apply(
+        get_label_from_given_ape_code)
     offerer_cultural_activity_dataframe.drop('APE_code', axis=1, inplace=True)
     return offerer_cultural_activity_dataframe.drop('siren', axis=1)
 
@@ -17,11 +23,6 @@ def get_label_from_given_ape_code(ape_code: str) -> str:
         return data.loc[data['Code NAF'] == ape_code].values[0][1]
     except IndexError:
         return ''
-
-
-MAINLAND_DEPARTEMENT_CODE_LENGTH = 2
-OVERSEAS_DEPARTEMENT_CODE_LENGTH = 3
-OVERSEAS_DEPARTEMENT_IDENTIFIER = '97'
 
 
 def get_departement_code(postalCode: str) -> str:
