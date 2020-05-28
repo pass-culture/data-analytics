d5481f188967be81fe22ad10083aac1ef07555ca
faustine <faustine@mbp-de-fam.home>
2020-05-28 19:29:50 +0200

    (PC-3679) Add enrich offer creation to create_enriched_data_views

--- write/create_enriched_data_views.py
+++ write/create_enriched_data_views.py
@@ -2,7 +2,7 @@ from flask import current_app as app
 from utils.logger import logger
 
 from write.create_views import create_enriched_offerer_data, create_enriched_user_data, \
-    create_enriched_stock_data
+    create_enriched_stock_data, create_enriched_offer_data
 
 def create_enriched_data_views():
     logger.info('[ENRICHED DATA] Start enriched data creation')
@@ -14,5 +14,7 @@ def create_enriched_data_views():
         logger.info('[ENRICHED DATA] Created enriched beneficiary user data')
         create_enriched_stock_data()
         logger.info('[ENRICHED DATA] Created enriched stock data')
+        create_enriched_offer_data()
+        logger.info('[ENRICHED DATA] Created enriched offer data')
 
     logger.info('[ENRICHED DATA] End enriched data creation')
