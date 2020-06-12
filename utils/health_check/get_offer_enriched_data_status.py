2aae96d514bbdec83d5aa4013ee959b038758772
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2020-06-12 19:06:37 +0200

    (PC-3784) corrected remaining errors on healthcheck

--- utils/health_check/get_offer_enriched_data_status.py
+++ utils/health_check/get_offer_enriched_data_status.py
@@ -9,7 +9,7 @@ def get_offer_enriched_data_status(
     enriched_status = dict()
     enriched_status.update({
         'is_enriched_offer_datasource_exists': is_enriched_offer_present(HealthCheckSession),
-        'is_offer_ok': is_enriched_offer_present(HealthCheckSession) and is_enriched_offers_contains_data(),
+        'is_offer_ok': is_enriched_offer_present(HealthCheckSession) and is_enriched_offers_contains_data(HealthCheckSession),
     })
 
     return enriched_status
