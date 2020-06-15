687cf3abeafb834ca1d22f72cd4123c958bc09b0
sofcalca <10255447+sofcalca@users.noreply.github.com>
2020-06-15 14:13:08 +0200

    (PC-3784) correct remaining healthcheck problems (#46)

--- utils/health_check/get_stock_enriched_data_status.py
+++ utils/health_check/get_stock_enriched_data_status.py
@@ -7,7 +7,8 @@ def get_stock_enriched_data_status(
     enriched_status = dict()
     enriched_status.update({
         'is_enriched_stock_datasource_exists': is_enriched_stock_data_exists(HealthCheckSession),
-        'is_stock_ok': is_enriched_stock_data_exists(HealthCheckSession) and is_enriched_stocks_contains_data()
+        'is_stock_ok': is_enriched_stock_data_exists(HealthCheckSession) and is_enriched_stocks_contains_data(
+            HealthCheckSession)
     })
 
     return enriched_status
--- utils/health_check/get_user_enriched_data_status.py
+++ utils/health_check/get_user_enriched_data_status.py
@@ -7,7 +7,8 @@ def get_user_enriched_data_status(
     enriched_status = dict()
     enriched_status.update({
         'is_enriched_user_datasource_exists': is_enriched_user_data_exists(HealthCheckSession),
-        'is_user_ok': is_enriched_user_data_exists(HealthCheckSession) and is_enriched_users_contains_data(),
+        'is_user_ok': is_enriched_user_data_exists(HealthCheckSession) and is_enriched_users_contains_data(
+            HealthCheckSession),
     })
 
     return enriched_status
