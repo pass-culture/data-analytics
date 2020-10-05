77d7d2c547a31b29b764740ad0b4cfa884e39084
Adrien Lin <adrien.lin@passculture.app>
2020-10-05 13:33:05 +0200

    (PC-4259) clean enriched_user_data table

--- write/create_intermediate_views_for_user.py
+++ write/create_intermediate_views_for_user.py
@@ -57,185 +57,106 @@ def _get_activation_dates_query() -> str:
     """
 
 
-def _get_first_connection_dates_query() -> str:
-    return """
-    (WITH recommendation_dates AS (
-     SELECT
-      MIN(recommendation."dateCreated") AS first_recommendation_date,
-      MAX(recommendation."dateCreated") AS last_recommendation_date,
-      "user".id AS user_id,
-      "user"."canBookFreeOffers"
-     FROM "user"
-     LEFT JOIN recommendation ON recommendation."userId" = "user".id
-     GROUP BY "user".id, "user"."canBookFreeOffers"
-    )
-
-    SELECT
-     recommendation_dates.first_recommendation_date AS "Date de première connexion",
-     recommendation_dates.user_id AS user_id
-    FROM recommendation_dates
-    WHERE recommendation_dates."canBookFreeOffers")
-    """
-
-
 def _get_date_of_first_bookings_query() -> str:
-    return """
-    WITH bookings_grouped_by_user AS (
-    SELECT
-     MIN(booking."dateCreated") AS date,
-     booking."userId" AS user_id
+    return '''
+     SELECT
+        booking."userId" AS user_id
+        ,min(booking."dateCreated") AS "Date de première réservation"
     FROM booking
     JOIN stock ON stock.id = booking."stockId"
-    JOIN offer
-     ON offer.id = stock."offerId"
-     AND offer.type != 'ThingType.ACTIVATION'
-    GROUP BY booking."userId"
-    )
-    SELECT bookings_grouped_by_user.date AS "Date de première réservation",
-    "user".id AS user_id
-    FROM "user"
-    LEFT JOIN bookings_grouped_by_user ON "user".id = bookings_grouped_by_user.user_id
-    WHERE "user"."canBookFreeOffers"
-    """
+    JOIN offer ON offer.id = stock."offerId"
+    AND offer.type != 'ThingType.ACTIVATION'
+    AND (offer."bookingEmail" != 'jeux-concours@passculture.app' OR offer."bookingEmail" IS NULL)
+    GROUP BY user_id
+    '''
 
 
 def _get_date_of_second_bookings_query() -> str:
-    return """
-     WITH second_booking_dates AS (
-     SELECT
-      ordered_dates."dateCreated" AS date,
-      ordered_dates."userId" AS user_id
-      FROM (
-       SELECT ROW_NUMBER()
-       OVER(
-        PARTITION BY "userId"
-        ORDER BY booking."dateCreated" ASC
-        ) AS rank, booking."dateCreated", booking."userId"
-       FROM booking
-       JOIN stock ON stock.id = booking."stockId"
-       JOIN offer ON offer.id = stock."offerId"
-       WHERE offer.type != 'ThingType.ACTIVATION'
-      ) AS ordered_dates
-      WHERE ordered_dates.rank = 2
+    return '''
+     WITH ranked_booking_data AS (
+         SELECT
+            booking."userId" AS user_id
+            ,booking."dateCreated" AS date_creation_booking
+            ,rank() over (partition by booking."userId" order by booking."dateCreated" asc) AS rank_booking
+           FROM booking
+           JOIN stock ON stock.id = booking."stockId"
+           JOIN offer ON offer.id = stock."offerId"
+           WHERE offer.type != 'ThingType.ACTIVATION'
+           AND (offer."bookingEmail" != 'jeux-concours@passculture.app' OR offer."bookingEmail" IS NULL)
     )
-
     SELECT
-     second_booking_dates.date AS "Date de deuxième réservation",
-     "user".id AS user_id
-    FROM "user"
-    LEFT JOIN second_booking_dates ON second_booking_dates.user_id = "user".id
-    WHERE "user"."canBookFreeOffers"
-    """
+        user_id
+        ,date_creation_booking AS "Date de deuxième réservation"
+    FROM ranked_booking_data
+    WHERE rank_booking = 2
+    '''
 
 
 def _get_date_of_bookings_on_third_product_type_query() -> str:
-    return """
-    WITH
-     bookings_on_distinct_types AS (
-      SELECT DISTINCT ON (offer.type, booking."userId") offer.type, booking."userId", booking."dateCreated"
-      FROM booking
-      JOIN stock ON stock.id = booking."stockId"
-      JOIN offer ON offer.id = stock."offerId"
-       AND offer.type != 'ThingType.ACTIVATION'
-      ORDER BY offer.type, booking."userId", booking."dateCreated" ASC
-     ),
-     first_booking_on_third_category AS (
-      SELECT
-       ordered_dates."dateCreated" AS date,
-       ordered_dates."userId"
-      FROM (
-       SELECT
-        ROW_NUMBER()
-         OVER(
-          PARTITION BY "userId"
-          ORDER BY bookings_on_distinct_types."dateCreated" ASC
-         ) AS rank, bookings_on_distinct_types."dateCreated",
-        bookings_on_distinct_types."userId"
-       FROM bookings_on_distinct_types
-          ) AS ordered_dates
-      WHERE ordered_dates.rank = 3
-    )
-
-      SELECT
-       first_booking_on_third_category.date AS "Date de première réservation dans 3 catégories différentes",
-       "user".id as user_id
-      FROM "user"
-      LEFT JOIN first_booking_on_third_category ON first_booking_on_third_category."userId" = "user".id
-      WHERE "user"."canBookFreeOffers"
-    """
-
-
-def _get_last_recommendation_dates_query() -> str:
-    return """
-    (WITH recommendation_dates AS (
-     SELECT
-      MIN(recommendation."dateCreated") AS first_recommendation_date,
-      MAX(recommendation."dateCreated") AS last_recommendation_date,
-      "user".id AS user_id,
-      "user"."canBookFreeOffers"
-     FROM "user"
-     LEFT JOIN recommendation ON recommendation."userId" = "user".id
-     GROUP BY "user".id, "user"."canBookFreeOffers"
-    )
-
+    return '''
+    WITH dat AS (
     SELECT
-     recommendation_dates.last_recommendation_date AS "Date de dernière recommandation",
-     recommendation_dates.user_id AS user_id
-    FROM recommendation_dates
-    WHERE recommendation_dates."canBookFreeOffers")
-    """
+        booking.*
+        ,offer."type" AS offer_type
+        ,offer."name" AS offer_name
+        ,offer.id AS offer_id
+        ,rank() over (partition by booking."userId",offer."type" order by booking."dateCreated") AS rank_booking_in_cat
+    FROM booking
+    JOIN stock
+    ON booking."stockId" = stock.id
+    JOIN offer
+    ON offer.id = stock."offerId"
+    WHERE offer."type" NOT IN ('ThingType.ACTIVATION','EventType.ACTIVATION')
+    AND (offer."bookingEmail" != 'jeux-concours@passculture.app' OR offer."bookingEmail" IS NULL)
+    ),
+    
+    ranked_data AS (
+    
+    SELECT
+        *
+        ,rank() over (partition by "userId" order by "dateCreated") AS rank_cat
+    FROM dat
+    WHERE rank_booking_in_cat = 1
+    )
+    
+    SELECT
+        "userId" AS user_id
+        ,"dateCreated" AS "Date de première réservation dans 3 catégories différentes"
+    FROM ranked_data
+    WHERE rank_cat = 3
+    '''
 
 
 def _get_number_of_bookings_query() -> str:
-    return """
-    (WITH bookings_grouped_by_user AS (
-     SELECT
-      MIN(booking."dateCreated") AS date,
-      SUM(booking.quantity) AS number_of_bookings,
-      "userId" AS user_id
-     FROM booking
-     JOIN stock ON stock.id = booking."stockId"
-     JOIN offer ON offer.id = stock."offerId"
-      AND offer.type != 'ThingType.ACTIVATION'
-     GROUP BY user_id
-    )
-
-    SELECT
-     CASE
-      WHEN bookings_grouped_by_user.number_of_bookings IS NULL THEN 0
-      ELSE bookings_grouped_by_user.number_of_bookings
-     END AS "Nombre de réservations totales",
-     "user".id AS user_id
-    FROM "user"
-    LEFT JOIN bookings_grouped_by_user ON "user".id = bookings_grouped_by_user.user_id
-    WHERE "user"."canBookFreeOffers")
-    """
+    return '''
+    SELECT  
+    booking."userId" AS user_id
+    ,count(booking.id) AS number_of_bookings
+    FROM booking
+    JOIN stock ON stock.id = booking."stockId"
+    JOIN offer ON offer.id = stock."offerId"
+    AND offer.type != 'ThingType.ACTIVATION'
+    AND (offer."bookingEmail" != 'jeux-concours@passculture.app' OR offer."bookingEmail" IS NULL)
+    GROUP BY user_id
+    ORDER BY number_of_bookings ASC
+    '''
 
 
 def _get_number_of_non_cancelled_bookings_query() -> str:
-    return """
-    (WITH non_cancelled_bookings_grouped_by_user AS(
-    SELECT
-     SUM(booking.quantity) AS number_of_bookings,
-     "userId" AS user_id
+    return '''
+    SELECT  
+        booking."userId" AS user_id
+        ,count(booking.id) AS number_of_bookings
+>>>>>>> (PC-4259) clean enriched_user_data table
     FROM booking
     JOIN stock ON stock.id = booking."stockId"
-    JOIN offer ON offer.id = stock."offerId" AND offer.type != 'ThingType.ACTIVATION'
-    WHERE
-     booking."isCancelled" IS FALSE
+    JOIN offer ON offer.id = stock."offerId"
+    AND offer.type != 'ThingType.ACTIVATION'
+    AND offer."bookingEmail" != 'jeux-concours@passculture.app'
+    AND NOT booking."isCancelled"
     GROUP BY user_id
-    )
-
-    SELECT
-     CASE
-      WHEN non_cancelled_bookings_grouped_by_user.number_of_bookings IS NULL THEN 0
-      ELSE non_cancelled_bookings_grouped_by_user.number_of_bookings
-    END AS "Nombre de réservations non annulées",
-     "user".id AS user_id
-    FROM "user"
-    LEFT JOIN non_cancelled_bookings_grouped_by_user ON non_cancelled_bookings_grouped_by_user.user_id = "user".id
-    WHERE "user"."canBookFreeOffers")
-    """
+    ORDER BY number_of_bookings ASC
+    '''
 
 
 def _get_users_seniority_query() -> str:
@@ -318,7 +239,6 @@ def _get_theoric_amount_spent_in_digital_goods_query() -> str:
     GROUP BY "user".id)
     """
 
-
 def _get_theoric_amount_spent_in_physical_goods_query() -> str:
     return """
     (WITH eligible_booking AS (
@@ -368,6 +288,19 @@ def _get_theoric_amount_spent_in_outings_query() -> str:
     GROUP BY "user".id)
     """
 
+def _get_last_booking_date_query() -> str:
+    return '''
+     SELECT
+        booking."userId" AS user_id
+        ,max(booking."dateCreated") AS "Date de dernière réservation"
+    FROM booking
+    JOIN stock ON stock.id = booking."stockId"
+    JOIN offer ON offer.id = stock."offerId" 
+    AND offer.type != 'ThingType.ACTIVATION'
+    AND (offer."bookingEmail" != 'jeux-concours@passculture.app' OR offer."bookingEmail" IS NULL)
+    GROUP BY user_id
+    '''
+
 
 def create_experimentation_sessions_view(ENGINE) -> None:
     view_query = f"""
@@ -380,14 +313,6 @@ def create_experimentation_sessions_view(ENGINE) -> None:
 def create_activation_dates_view(ENGINE) -> None:
     view_query = f"""
         CREATE OR REPLACE VIEW activation_dates AS {_get_activation_dates_query()} 
-        """
-    with ENGINE.connect() as connection:
-        connection.execute(view_query)
-
-
-def create_first_connection_dates_view(ENGINE) -> None:
-    view_query = f"""
-        CREATE OR REPLACE VIEW first_connection_dates AS {_get_first_connection_dates_query()} 
         """
     with ENGINE.connect() as connection:
         connection.execute(view_query)
@@ -412,14 +337,6 @@ def create_date_of_second_bookings_view(ENGINE) -> None:
 def create_date_of_bookings_on_third_product_view(ENGINE) -> None:
     view_query = f"""
         CREATE OR REPLACE VIEW date_of_bookings_on_third_product AS {_get_date_of_bookings_on_third_product_type_query()} 
-        """
-    with ENGINE.connect() as connection:
-        connection.execute(view_query)
-
-
-def create_last_recommendation_dates_view(ENGINE) -> None:
-    view_query = f"""
-        CREATE OR REPLACE VIEW last_recommendation_dates AS {_get_last_recommendation_dates_query()} 
         """
     with ENGINE.connect() as connection:
         connection.execute(view_query)
@@ -488,6 +405,13 @@ def create_theoric_amount_spent_in_outings_view(ENGINE) -> None:
     with ENGINE.connect() as connection:
         connection.execute(view_query)
 
+def create_date_of_last_booking_view(ENGINE) -> None:
+    view_query = f'''
+        CREATE OR REPLACE VIEW last_booking_date AS {_get_last_booking_date_query()} 
+        '''
+    with ENGINE.connect() as connection:
+        connection.execute(view_query)
+
 
 def create_materialized_enriched_user_view(ENGINE) -> None:
     query = """
@@ -499,14 +423,12 @@ def create_materialized_enriched_user_view(ENGINE) -> None:
          "user"."postalCode" AS "Code postal",
          "user".activity AS "Statut",
          activation_dates."Date d'activation",
-         "user"."culturalSurveyFilledDate" AS "Date de remplissage du typeform",
-         first_connection_dates."Date de première connexion",
+         CASE WHEN "user"."hasSeenTutorials" THEN "user"."culturalSurveyFilledDate" ELSE NULL END AS "Date de première connexion",
          date_of_first_bookings."Date de première réservation",
          date_of_second_bookings."Date de deuxième réservation",
          date_of_bookings_on_third_product."Date de première réservation dans 3 catégories différentes",
-         last_recommendation_dates."Date de dernière recommandation",
-         number_of_bookings."Nombre de réservations totales",
-         number_of_non_cancelled_bookings."Nombre de réservations non annulées",
+         COALESCE(number_of_bookings.number_of_bookings,0) AS "Nombre de réservations totales",
+         COALESCE(number_of_non_cancelled_bookings.number_of_bookings,0) AS "Nombre de réservations non annulées",
          users_seniority."Ancienneté en jours",
          actual_amount_spent."Montant réél dépensé",
          theoric_amount_spent."Montant théorique dépensé",
@@ -514,22 +436,22 @@ def create_materialized_enriched_user_view(ENGINE) -> None:
          theoric_amount_spent_in_physical_goods."Dépenses physiques",
          theoric_amount_spent_in_outings."Dépenses sorties",
          user_humanized_id.humanized_id AS "user_humanized_id"
+         last_booking_date."Date de dernière réservation"
         FROM "user"
         LEFT JOIN experimentation_sessions ON "user".id = experimentation_sessions."user_id"
-        LEFT JOIN activation_dates ON experimentation_sessions.user_id = activation_dates.user_id
-        LEFT JOIN first_connection_dates ON activation_dates.user_id = first_connection_dates.user_id
-        LEFT JOIN date_of_first_bookings ON first_connection_dates.user_id = date_of_first_bookings.user_id
-        LEFT JOIN date_of_second_bookings ON date_of_first_bookings.user_id = date_of_second_bookings.user_id
-        LEFT JOIN date_of_bookings_on_third_product ON date_of_second_bookings.user_id = date_of_bookings_on_third_product.user_id
-        LEFT JOIN last_recommendation_dates ON date_of_bookings_on_third_product.user_id = last_recommendation_dates.user_id
-        LEFT JOIN number_of_bookings ON last_recommendation_dates.user_id = number_of_bookings.user_id
-        LEFT JOIN number_of_non_cancelled_bookings ON number_of_bookings.user_id = number_of_non_cancelled_bookings.user_id
-        LEFT JOIN users_seniority ON number_of_non_cancelled_bookings.user_id = users_seniority.user_id
-        LEFT JOIN actual_amount_spent ON users_seniority.user_id = actual_amount_spent.user_id
-        LEFT JOIN theoric_amount_spent ON actual_amount_spent.user_id = theoric_amount_spent.user_id
-        LEFT JOIN theoric_amount_spent_in_digital_goods ON theoric_amount_spent.user_id = theoric_amount_spent_in_digital_goods.user_id
-        LEFT JOIN theoric_amount_spent_in_physical_goods ON theoric_amount_spent_in_digital_goods.user_id = theoric_amount_spent_in_physical_goods.user_id
-        LEFT JOIN theoric_amount_spent_in_outings ON theoric_amount_spent_in_physical_goods.user_id = theoric_amount_spent_in_outings.user_id
+        LEFT JOIN activation_dates ON "user".id  = activation_dates.user_id
+        LEFT JOIN date_of_first_bookings ON "user".id  = date_of_first_bookings.user_id
+        LEFT JOIN date_of_second_bookings ON "user".id  = date_of_second_bookings.user_id
+        LEFT JOIN date_of_bookings_on_third_product ON "user".id = date_of_bookings_on_third_product.user_id
+        LEFT JOIN number_of_bookings ON "user".id = number_of_bookings.user_id
+        LEFT JOIN number_of_non_cancelled_bookings ON "user".id = number_of_non_cancelled_bookings.user_id
+        LEFT JOIN users_seniority ON "user".id = users_seniority.user_id
+        LEFT JOIN actual_amount_spent ON "user".id = actual_amount_spent.user_id
+        LEFT JOIN theoric_amount_spent ON "user".id = theoric_amount_spent.user_id
+        LEFT JOIN theoric_amount_spent_in_digital_goods ON "user".id  = theoric_amount_spent_in_digital_goods.user_id
+        LEFT JOIN theoric_amount_spent_in_physical_goods ON "user".id = theoric_amount_spent_in_physical_goods.user_id
+        LEFT JOIN theoric_amount_spent_in_outings ON "user".id = theoric_amount_spent_in_outings.user_id
+        LEFT JOIN last_booking_date ON last_booking_date.user_id = "user".id
         LEFT JOIN user_humanized_id ON user_humanized_id.id = "user".id
         WHERE "user"."canBookFreeOffers");
         """
