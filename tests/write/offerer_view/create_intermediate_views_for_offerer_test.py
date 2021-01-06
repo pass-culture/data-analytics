7ec3c0c9ad68ba8d7e5bf72204cc60246f32014a
FerdiHr <ferdi.harmel@gmail.com>
2021-01-06 17:21:41 +0100

    (PC-6029) Fix test for next year ;)

--- tests/write/offerer_view/create_intermediate_views_for_offerer_test.py
+++ tests/write/offerer_view/create_intermediate_views_for_offerer_test.py
@@ -317,7 +317,7 @@ class OffererQueriesTest:
                 stock_id=20,
                 token="ABC321",
                 is_used=True,
-                date_created='2021-01-05'
+                date_created=f'{datetime.now().year}-01-05'
             )
             create_booking(
                 id=3,
